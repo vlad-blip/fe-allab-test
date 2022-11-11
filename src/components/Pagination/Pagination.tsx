cbd3c41e4c4a0e8e3cc20b4efbedfb4d6977c662
Vladyslav Lazarenko <ezio2506@gmail.com>
2022-11-11 18:31:40 +0200

    Fixed paggination size on small screens

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -4,9 +4,9 @@ import { useEffect } from 'react';
 
 export default function Pagination() {
     return (
-        <nav className='flex items-center max-w-fit text-[1.6rem] xl:text-[2rem] font-bold bg-white text-blue-600 shadow-pagination rounded-2xl py-4 px-10 mx-auto'>
+        <nav className='flex items-center max-w-fit text-[1.6rem] xl:text-[2rem] font-bold bg-white text-blue-600 shadow-pagination rounded-2xl py-4 px-4 xl:px-10 mx-auto'>
             <a href="#" className='hidden xl:block'><ArrowLeft/></a>
-            <ul className='flex gap-4 px-8 xl:px-12 mx-12 border-x border-white-500'>
+            <ul className='flex gap-4 px-8 xl:px-12 mx-8 xl:mx-12 border-x border-white-500'>
                 <ButtonPage isActive>1</ButtonPage>
                 <ButtonPage>2</ButtonPage>
                 <ButtonPage>3</ButtonPage>
